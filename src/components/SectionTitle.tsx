b021b12adfcfbecea4c7a462b8bfb09ff46d2769
Acox <snsdkaskusone@gmail.com>
2025-10-16 12:29:31 +0700

    feat: change favicon

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -7,10 +7,10 @@ interface Props {
 }
 
 const SectionTitle: React.FC<Props> = ({ children, permanent }) => {
-    return <div className={`${permanent ? "block" : "block lg:hidden"} sticky top-0 py-4 bg-background-dark backdrop-blur w-full z-3 max-w-dwh overflow-hidden`}>
-        <div className="w-dvw h-full absolute right-0 top-0 translate-x-full bg-background-dark" />
+    return <div className={`${permanent ? "block" : "block lg:hidden"} sticky top-0 py-4 bg-background/90 backdrop-blur w-full z-3 max-w-dwh overflow-hidden`}>
+        <div className="w-dvw h-full absolute right-0 top-0 translate-x-full bg-background/90" />
         <label className="text-primary font-semibold text-base lg:text-lg">{children}</label>
-        <div className="w-dvw h-full absolute left-0 top-0 -translate-x-full bg-background-dark" />
+        <div className="w-dvw h-full absolute left-0 top-0 -translate-x-full bg-background/90" />
     </div>
 }
 
