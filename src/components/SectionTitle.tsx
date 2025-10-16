8fe80842715af67640cb0064b555b823733423de
Acox <snsdkaskusone@gmail.com>
2025-10-16 12:45:36 +0700

    chore: revert section-title bg

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -7,10 +7,8 @@ interface Props {
 }
 
 const SectionTitle: React.FC<Props> = ({ children, permanent }) => {
-    return <div className={`${permanent ? "block" : "block lg:hidden"} sticky top-0 py-4 bg-background/90 backdrop-blur w-full z-3 max-w-dwh overflow-hidden`}>
-        <div className="w-dvw h-full absolute right-0 top-0 translate-x-full bg-background/90" />
+    return <div className={`${permanent ? "block" : "block lg:hidden"} sticky top-0 py-4 backdrop-blur w-full z-3 max-w-dwh`}>
         <label className="text-primary font-semibold text-base lg:text-lg">{children}</label>
-        <div className="w-dvw h-full absolute left-0 top-0 -translate-x-full bg-background/90" />
     </div>
 }
 
